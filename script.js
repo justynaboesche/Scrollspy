29f610ca7dfb0ec50b1c9774c90bded18a104753
Justyna Boesche <justyna.boesche@gmail.com>
2024-03-23 09:24:17 +0100

    function handleScrollSpy created

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-console.log('Nic tu nie ma 😱')
+const menuItems = document.querySelectorAll('a');
+const scrollSpySections = document.querySelectorAll('.section');
+
+const handleScrollSpy = () => {
+	if (document.body.classList.contains('main-page')) {
+		const sections = [];
+
+		scrollSpySections.forEach(section => {
+			// console.log(window.scrollY);
+			// console.log(section.offsetTop);
+			// console.log(section.offsetHeight);
+
+			if (window.scrollY <= section.offsetTop + section.offsetHeight -102.67) {
+				sections.push(section);
+
+
+                const activeSection = document.querySelector(`[href*="${sections[0].id}"`)
+
+                menuItems.forEach(item => {
+                    item.classList.remove('active')
+                })
+
+                activeSection.classList.add('active')
+			}
+
+
+            if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 200) {
+                const lastSection = document.querySelector('a:last-of-type')
+
+				menuItems.forEach(item => {
+                    item.classList.remove('active')
+                })
+
+                lastSection.classList.add('active')
+            }
+		});
+	}
+};
+
+window.addEventListener('scroll', handleScrollSpy);
+
+
+
+// const sections = document.querySelectorAll('.section');
+// const menuItems = document.querySelectorAll('a');
+ 
+// const options = {
+//     threshold: [0.5, 0.9]
+// };
+ 
+// const handleScrollspy = entries => {
+//     entries.forEach(entry => {
+//         const activeNav = document.querySelector(`a[href='#${entry.target.id}']`);
+ 
+//         if(entry.isIntersecting) {
+//             menuItems.forEach(item => item.classList.remove('active'));
+//             activeNav.classList.add('active');
+//         };
+
+//     });
+// };
+ 
+// const observer = new IntersectionObserver(handleScrollspy, options);
+ 
+// sections.forEach(section => {
+//     observer.observe(section);
+// });
